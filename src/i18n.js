bda681fbe3f575a7341abfac54fe4288e33ff6e3
marduzca <arduz.miguel@gmail.com>
2020-06-04 18:11:15 +0200

    Go back to legacy version of react-i19next

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,5 @@
-import i18n from "i18next";
-import { reactI18nextModule } from "react-i18next";
+import i18n from 'i18next';
+import { reactI18nextModule } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 
 import translationEN from './locales/en/translation.json';
