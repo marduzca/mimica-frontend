804e64d94a94e2aef1b59b88932fbfd89a89cb9b
marduzca <arduz.miguel@gmail.com>
2020-06-08 13:12:18 +0200

    Extract settings options to array

--- src/components/waiting-room/settings/Settings.js
+++ src/components/waiting-room/settings/Settings.js
@@ -4,6 +4,7 @@ import { withNamespaces } from 'react-i18next';
 import './Settings.css';
 
 const roundsOptions = [3, 4, 5, 6, 7];
+const timesOptions = [30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
 
 function Settings(props) {
     const { t } = props;
@@ -32,16 +33,7 @@ function Settings(props) {
 
                 <label htmlFor="time">{t('Time in seconds')}</label>
                 <select id="time" defaultValue="80" onChange={handleTimeChange}>
-                    <option value="30">30</option>
-                    <option value="40">40</option>
-                    <option value="50">50</option>
-                    <option value="60">60</option>
-                    <option value="70">70</option>
-                    <option value="80">80</option>
-                    <option value="90">90</option>
-                    <option value="100">100</option>
-                    <option value="110">110</option>
-                    <option value="120">120</option>
+                    {timesOptions.map(opt => <option key={opt} value={opt}>{opt}</option>)};
                 </select>
 
                 <p>{t('Language', { language: props.language })}</p>
