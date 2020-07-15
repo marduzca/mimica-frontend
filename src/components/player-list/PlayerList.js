0129c71e0721629eb48ae28291581ca7ebdb0e28
marduzca <arduz.miguel@gmail.com>
2020-07-15 12:29:03 +0200

    Display list of players received from Waiting room instead of hardcoded version

--- src/components/player-list/PlayerList.js
+++ src/components/player-list/PlayerList.js
@@ -6,38 +6,11 @@ import Player from './player/Player';
 import './PlayerList.css';
 
 function PlayerList(props) {
-    const players = [
-        {
-            name: props.playerName,
-            points: 100,
-            position: 1
-        },
-        {
-            name: 'Player 2',
-            points: 80,
-            position: 2
-        },
-        {
-            name: 'Player 3',
-            points: 50,
-            position: 3
-        },
-        {
-            name: 'Player 4',
-            points: 30,
-            position: 4
-        },
-        {
-            name: 'Player 5',
-            points: 10,
-            position: 5
-        }
-    ];
 
     const fillList = () => {
-        return players.map(player => {
+        return props.currentPlayers.map(player => {
             return (
-                <Player key={player.name} name={player.name} points={player.points} position={player.position} inGame={props.inGame} />
+                <Player key={player.name} name={player.name} isHost={player.host} points={800} position={1} inGame={props.inGame} />
             )
         });
     }
@@ -51,7 +24,7 @@ function PlayerList(props) {
 
 PlayerList.propTypes = {
     inGame: PropTypes.bool.isRequired,
-    playerName: PropTypes.string.isRequired
+    currentPlayers: PropTypes.array.isRequired
 };
 
 export default PlayerList;
--- src/components/player-list/PlayerList.test.js
+++ src/components/player-list/PlayerList.test.js
@@ -4,14 +4,14 @@ import { render, screen } from '@testing-library/react';
 import PlayerList from './PlayerList';
 
 test('Player list displays position and points during game', () => {
-    render(<PlayerList playerName={'Miguel'} inGame={true}/>);
+    render(<PlayerList currentPlayers={[{ name: 'Miguel', host: true }]} inGame={true} />);
 
     expect(screen.getAllByTestId('position-text')[0]).toBeInTheDocument();
     expect(screen.getAllByTestId('points-text')[0]).toBeInTheDocument();
 });
 
 test('Player list doesn\'t display position and points in waiting room', () => {
-    render(<PlayerList playerName={'Miguel'} inGame={false}/>);
+    render(<PlayerList currentPlayers={[{ name: 'Miguel', host: true }]} inGame={false} />);
 
     expect(screen.queryByTestId('position-text')).not.toBeInTheDocument();
     expect(screen.queryByTestId('points-text')).not.toBeInTheDocument();
