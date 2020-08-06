cd4affbfd7c7f1237fe4c3bf17293da4bcd30837
marduzca <arduz.miguel@gmail.com>
2020-08-06 17:18:50 +0200

    Add test to check general behavior of player list

--- src/components/player-list/PlayerList.test.js
+++ src/components/player-list/PlayerList.test.js
@@ -3,18 +3,20 @@ import { render, screen, within } from '@testing-library/react';
 
 import PlayerList from './PlayerList';
 
-test('Player list displays position and points during game', () => {
-    render(<PlayerList currentPlayers={[{ name: 'Miguel', host: true }]} inGame={true} />);
+test('Player list displays all players', () => {
+    render(<PlayerList currentPlayers={
+        [{
+            name: 'Miguel',
+            host: true
+        },
+        {
+            name: 'Friend',
+            host: false
+        }]
+    } inGame={false} />);
 
-    expect(screen.getAllByTestId('position-text')[0]).toBeInTheDocument();
-    expect(screen.getAllByTestId('points-text')[0]).toBeInTheDocument();
-});
-
-test('Player list doesn\'t display position and points in waiting room', () => {
-    render(<PlayerList currentPlayers={[{ name: 'Miguel', host: true }]} inGame={false} />);
-
-    expect(screen.queryByTestId('position-text')).not.toBeInTheDocument();
-    expect(screen.queryByTestId('points-text')).not.toBeInTheDocument();
+    expect(screen.getByTestId(/miguel/i)).toBeInTheDocument();
+    expect(screen.getByTestId(/friend/i)).toBeInTheDocument();
 });
 
 test('Player list properly shows who is host and who is not', () => {
@@ -35,4 +37,19 @@ test('Player list properly shows who is host and who is not', () => {
     expect(within(hostPlayer).queryByAltText(/Host/i)).toBeInTheDocument();
     expect(within(nonHostPlayer).queryByAltText(/Host/i)).not.toBeInTheDocument();
 });
+
+test('Player list displays position and points during game', () => {
+    render(<PlayerList currentPlayers={[{ name: 'Miguel', host: true }]} inGame={true} />);
+
+    expect(screen.getAllByTestId('position-text')[0]).toBeInTheDocument();
+    expect(screen.getAllByTestId('points-text')[0]).toBeInTheDocument();
+});
+
+test('Player list doesn\'t display position and points in waiting room', () => {
+    render(<PlayerList currentPlayers={[{ name: 'Miguel', host: true }]} inGame={false} />);
+
+    expect(screen.queryByTestId('position-text')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('points-text')).not.toBeInTheDocument();
+});
+
 
