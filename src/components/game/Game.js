d7ab39b007c0c4267cfdf5903c9611c698984938
marduzca <arduz.miguel@gmail.com>
2020-08-14 15:32:34 +0200

    Make broadcast runnable

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 
-import GameBar from './gamebar/GameBar';
-import PlayerList from '../player-list/PlayerList';
+// import GameBar from './gamebar/GameBar';
+// import PlayerList from '../player-list/PlayerList';
 import VideoCamera from './videocam/VideoCamera';
 import Chat from './chat/Chat';
 import socket from '../../webSocket';
@@ -10,21 +10,34 @@ import socket from '../../webSocket';
 import './Game.css';
 
 function Game(props) {
-    const [currentPlayers, setCurrentPlayers] = useState(props.location.state.currentPlayers);
+    // const [currentPlayers, setCurrentPlayers] = useState(props.location.state.currentPlayers);
+    const isHost = () => {
+        const query = new URLSearchParams(props.location.search);
+
+        for (let param of query.entries()) {
+            if (param[0]) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
 
     useEffect(() => {
         socket.on('currentPlayers', (players) => {
-            setCurrentPlayers(players);
+            // setCurrentPlayers(players);
         });
 
-    }, [currentPlayers]);
+    }, []);
 
     return (
         <div className="game">
-            <GameBar time={props.location.state.time} numberOfRounds={props.location.state.numberOfRounds}/>
+            {/*<GameBar time={props.location.state.time} numberOfRounds={props.location.state.numberOfRounds}/>*/}
             <div className="gameplay">
-                <PlayerList currentPlayers={currentPlayers} inGame={true}/>
-                <VideoCamera host={props.location.state.isHost} currentPlayers={currentPlayers} roomID={props.location.state.roomID} />
+                {/*<PlayerList currentPlayers={currentPlayers} inGame={true}/>*/}
+                {/*<VideoCamera host={props.location.state.isHost} currentPlayers={currentPlayers} roomID={props.location.state.roomID} />*/}
+                <VideoCamera host={isHost()} />
                 <Chat/>
             </div>
         </div>
